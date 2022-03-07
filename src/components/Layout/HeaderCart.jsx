da115e565b7c40df19b642d71231dabcc830644d
WoojaeJang <staphano77x@naver.com>
2022-03-07 17:04:47 +0900

    [2020-03-07 start]

---
+++ src/components/Layout/HeaderCart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './HeaderCart.module.css'
+import CartIcon from '../Cart/CartIcon'
+
+const HeaderCart = () => {
+    // <button class="button"></button>
+  return (
+    <button className={classes.button}>
+        <span className={classes.icon}><CartIcon /></span>
+        <span>내 장바구니</span>
+    </button>
+  )
+}
+
+export default HeaderCart
